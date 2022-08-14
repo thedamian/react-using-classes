a0edb34ca5b875ccfb9a7f043c1a19a9e7bedf54
Damian Montero <photoca@hotmail.com>
2022-08-14 17:01:11 -0400

    Adding props for class

---
+++ src/ClassComponent.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class ClassComponent extends Component {
+    
+    constructor(props) {
+        super(props)
+        console.log(props)
+        this.state = {
+            count: props.count
+        }
+    }
+
+    render() {
+        return <>
+        <h1>Welcome to class Components {this.state.count}</h1>
+        </>
+    }
+}
+
+export default ClassComponent
---
+++ src/Function.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Function() {
+
+    return (
+        <>
+        <h1>Hello there functions!</h1>
+        </>
+    )
+}
+
+export default Function
