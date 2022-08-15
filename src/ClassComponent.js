06ab1671ccee7d73850b9607533c9a7c1063dac4
Damian Montero <photoca@hotmail.com>
2022-08-15 10:51:06 -0400

    adding useEffect

--- src/ClassComponent.js
+++ src/ClassComponent.js
@@ -4,19 +4,40 @@ class ClassComponent extends Component {
     
     constructor(props) {
         super(props)
-        //console.log(props)
+        console.log(props)
         this.state = {
-            count: props.count
+            count: props.count,
+            coffee: []
         }
     }
+
+    componentDidMount() {
+        // Runs after the first render() lifecycle
+       fetch("https://api.sampleapis.com/coffee/hot")
+       .then(res=>res.json())
+       .then(data => {
+        this.setState({ 
+            count: this.state.count,
+            coffee: data
+            })
+       })
+
+      }
+    
 
     render() {
         return <>
         <h1>Class Component</h1>
         <p> Count is: {this.state.count}</p>
-        <p><button onClick={()=> {this.setState({ 
-            count: this.state.count + 1 
-            })}}>Add one</button></p>
+        <p>
+            <button onClick={()=> {this.setState({   
+                count: this.state.count + 1,
+                coffee: this.state.coffee
+                })}}>Add one</button>
+        </p>
+        <p>
+        {this.state.coffee.map(c=> c.title).join(" - ")}
+        </p>
         </>
     }
 }
